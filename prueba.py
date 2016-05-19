af49e542a3d5140fd798cae2b43148a499004a44
José Ruiz <jcrcsh@gmail.com>
2016-05-19 07:20:16 -0500

    prueba.py
    1.2

--- prueba.py
+++ prueba.py
@@ -6,10 +6,12 @@ def menu():
 
 def suma():
     x1 = int(input("Ingrese el valor de x1: "))
-    x1 = int(input("Ingrese el valor de x2: "))
-    x1 = int(input("Ingrese el valor de x3: "))
-    x1 = int(input("Ingrese el valor de y1: "))
-    x1 = int(input("Ingrese el valor de y2: "))
-    x1 = int(input("Ingrese el valor de y3: "))
-    
+    x2 = int(input("Ingrese el valor de x2: "))
+    y1 = int(input("Ingrese el valor de y1: "))
+    y2 = int(input("Ingrese el valor de y2: "))
+    z1 = int(input("Ingrese el valor de z1: "))
+    z2 = int(input("Ingrese el valor de z2: "))
+    coor1 = x1 + x2
+    coor2 = y1 + y2
+    coor3 = z1 + z2
 menu()
