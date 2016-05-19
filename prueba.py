58ea1b750910468897bdc20559f12b71437d7977
José Ruiz <jcrcsh@gmail.com>
2016-05-19 07:42:41 -0500

    prueba.py
    1.6

--- prueba.py
+++ prueba.py
@@ -29,18 +29,18 @@ def suma():
     print("z: " + str(coor3))
 
 def promedio()
-	s= int(1)
-	contador= int(0)
-	suma= int(0)
-	resultado= int(0)
-		while(s==1):
-			r=int(input("Ingrese un numero\n"))
-			suma+=r
-			contador+=1
-			if (r==0):
-				s=0
-			resultado= suma/contador
-		print (" EL promedio es: "+str(resultado))
+	s = int(1)
+	contador = int(0)
+	suma = int(0)
+	resultado = int(0)
+		while(s == 1):
+			r = int(input("Ingrese un numero\n"))
+			suma += r
+			contador += 1
+			if (r == 0):
+				s = 0
+			resultado = suma / contador
+		print ("EL promedio es: " + str(resultado))
 
 
 
