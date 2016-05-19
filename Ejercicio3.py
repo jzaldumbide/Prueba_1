db0f95a958483a9dfc27d71c4d4329ece4c76c80
ajdiaz105 <ajdiaz105@gmail.com>
2016-05-19 07:38:56 -0500

    avance

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -1,15 +1,19 @@
+def reverse(list):
+    if len(list)==1:
+        return list
+    else:
+        return list[-1]+reverse(list[:-1])
+
 def leertxt():
     archi=open('texto.txt','r')
     linea=archi.readline()
     while linea!="":
         print (linea)
         linea=archi.readline()
-        frase=linea
+        frase=list(str(linea))
+        print(reverse (str(frase)))
         archi.close()
         print (linea)
     archi.close()
-    linea = linea.split()
-    nlinea = len(linea)
-    print (nlinea)
-
 leertxt()
+
