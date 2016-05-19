410599e958369d5e5088665df38636928a6883d0
José Ruiz <jcrcsh@gmail.com>
2016-05-19 07:17:52 -0500

    prueba.py
    1.1

--- prueba.py
+++ prueba.py
@@ -4,6 +4,12 @@
 def menu():
     print("Prueba Programacion Avanzada")
 
-
-
+def suma():
+    x1 = int(input("Ingrese el valor de x1: "))
+    x1 = int(input("Ingrese el valor de x2: "))
+    x1 = int(input("Ingrese el valor de x3: "))
+    x1 = int(input("Ingrese el valor de y1: "))
+    x1 = int(input("Ingrese el valor de y2: "))
+    x1 = int(input("Ingrese el valor de y3: "))
+    
 menu()
